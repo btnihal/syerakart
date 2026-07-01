c7e013a7225e02a76eb54791638af19ac3f8a2c9
nihalbt <mhmdnhl001@gmail.com>
2026-07-02 00:00:50 +0530

    Add allowed host for Railway

--- SyeraKart/settings.py
+++ SyeraKart/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['syerakart.up.railway.app', 'syerakart.com']
+ALLOWED_HOSTS = ['syerakart-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
